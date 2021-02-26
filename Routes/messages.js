4b8e7cdf39f7aa2bf15187e9608fc7c72e55e6ba
Elie <eliegebransoued@gmail.com>
2021-02-26 11:01:05 +0100

    creating the routes + creating the DBConfig

---
+++ Routes/messages.js
@@ -0,0 +1,22 @@
+const pool = require("../DBConfig/dbconfig");
+const express = require ("express");
+const router = express.Router();
+
+router.get("/messages", 
+
+    async (_, res) => {
+    try {
+      const dbResponse = await pool.query('SELECT * FROM messages');
+      res.json({
+        code: 200,
+        message: "success",
+        data: dbResponse.rows,
+      });
+    } catch (e) {
+      console.error(Error(e));
+      res.status(500);
+    }
+  })
+
+
+  module.exports= router;
--- server.js
+++ server.js
@@ -1,5 +1,14 @@
+// Module import
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require ("express");
 const app = express();
 
+//Route Import
+const usersRoute = require("./Routes/users");
+const messagesRoute = require("./Routes/messages");
+
+app.use("/", usersRoute);
+app.use("/", messagesRoute);
 
 app.listen(3000, () => console.log("server is listening on port 3000"));
